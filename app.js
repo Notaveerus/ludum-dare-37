31d75a72583e9adb0e7ae4613eedf92865afd20c
Tyler <pieninja95@gmail.com>
2016-12-11 23:40:04 +1000

    initial ludum commit -bugfixes required

---
+++ app.js
@@ -0,0 +1,199 @@
+var monsters = [];
+var heroes = [];
+var gold = 140;
+var door;
+var wave = 0;
+var waiting = true;
+var goldCount;
+var clock;
+var enemyCount=0;
+var store = [
+  {
+    value: 20,
+    name: 'fighter'
+  },
+  {
+    value:20,
+    name: 'ranger'
+  },
+  {
+    value: 40,
+    name: 'barbarian'
+  },
+  {
+    value: 30,
+    name: 'cleric'
+  },
+  {
+    value: 1,
+    name: 'peasant'
+  }
+]
+var abilities = [
+  {
+    value: 5,
+    name: 'shield'
+  },
+  {
+    value: 10,
+    name: 'heal'
+  }
+]
+var placeIndex = 0;
+var countTime = 25
+var assetsObj = {
+  "images":['cleric.png','fighter.png','ranger.png','barbarian.png']
+
+  }
+
+$(document).ready(function(){
+  Crafty.init(window.innerWidth-25,window.innerHeight-25);
+  var types = ['peasant','cleric','fighter','ranger','barbarian']
+
+
+  var initParty = function(){
+    var offset = -120
+    var diffMod = 1+(wave/10);
+    for(var i=0;i<4;i++){
+      enemyCount++
+      var hero = Crafty.e('AI')
+      .attr({x:15,y:Crafty.viewport.height/2+offset})
+      hero.type = types[Crafty.math.randomInt(0,types.length-1)]
+      if(hero.type == 'peasant'){
+        offset+=45
+        var peasant = Crafty.e('AI')
+        .attr({x:50,y:Crafty.viewport.height/2+offset})
+        peasant.type = 'peasant'
+        peasant.team = 'hero'
+        peasant.diffMod = diffMod
+        heroes.push(peasant)
+
+      }
+      hero.team = 'hero';
+      hero.diffMod = diffMod;
+      offset+=45
+      heroes.push(hero)
+    }
+    wave+=1;
+    placeIndex = 0;
+    waiting = true;
+  }
+
+
+  Crafty.scene("main",function(){
+    clock = Crafty.e("Countdown");
+    Crafty.background('url(floor.png) repeat');
+    Crafty.bind('KeyDown',function(e){
+      if(e.key == Crafty.keys.R){
+        monsters.length = 0;
+        heroes.length = 0;
+        gold = 140;
+        wave = 0;
+        waiting = true;
+        enemyCount=0;
+        placeIndex = 0;
+        countTime=25;
+        Crafty.scene('main')
+        if(Crafty.isPaused())
+          Crafty.pause();
+
+      }
+    })
+    var screen = Crafty.e("2D, Mouse, DOM")
+    .attr({w:window.innerWidth-25,h:window.innerHeight-25,x:0,y:0})
+    .bind('MouseDown',function(e){
+      if(e.mouseButton === Crafty.mouseButtons.LEFT){
+        if(!heroes.length){
+          if(store[placeIndex].value<=gold){
+            var monster =Crafty.e("AI")
+            monster.x = Crafty.mousePos.x;
+            monster.y = Crafty.mousePos.y;
+            monster.spawn = {
+              x:monster.x,
+              y:monster.y
+            }
+            monster.type = store[placeIndex].name;
+            monster.team = 'monster'
+
+            gold-=store[placeIndex].value;
+            monsters.push(monster)
+          }
+        }
+
+
+      }
+      if(e.mouseButton == Crafty.mouseButtons.RIGHT){
+
+        var enemy =Crafty.e("AI")
+        enemy.x = Crafty.mousePos.x;
+        enemy.y = Crafty.mousePos.y
+        enemy.type = store[placeIndex].name;
+        enemy.team = 'hero'
+          placeIndex = 0;
+        heroes.push(enemy)
+      }
+
+    })
+    .bind('MouseWheelScroll',function(evt){
+      placeIndex-= evt.direction;
+
+      if(placeIndex<0)
+        placeIndex = 0;
+      if(placeIndex>store.length-1 && heroes.length===0)
+        placeIndex = store.length-1
+      else if(placeIndex>abilities.length-1&&heroes.length>0)
+        placeIndex = abilities.length-1
+
+    })
+    .bind("EnterFrame",function(){
+      gold = parseInt(gold)
+      goldCount.gold = gold;
+      console.log(heroes.length,waiting)
+      if(!heroes.length && waiting){
+        waiting = false;
+        clock.value=countTime;
+        clock.tick();
+        Crafty.e("Delay").delay(function(){
+          initParty();
+
+        },countTime*1000)}
+
+  })
+  goldCount = Crafty.e('Gold')
+  var selected = Crafty.e('Selected')
+
+
+  goldCount.gold = gold;
+  door = Crafty.e("2D,DOM,Color,Collision").attr({x:Crafty.viewport.width-15,y:Crafty.viewport.height/2-50,w:30,h:100})
+  .color('blue')
+  door.health=500
+  door.healthBar = Crafty.e('Status').attr({x:Crafty.viewport.width/2-150,y:15,w:300,h:20})
+  door.healthBar.maxWidth = 300;
+  door.checkHits('Arrow,Attack')
+  door.bind('HitOn', function(hitData){
+    for(var i=0;i<hitData.length;i++){
+      this.health -= hitData[i].obj.damage;
+      this.healthBar.subtract(hitData[i].obj.damage,500)
+      hitData[i].obj.destroy();
+      if(this.health<=0){
+        door.destroy()
+        var gameOver = Crafty.e('2D,DOM,Text')
+        .attr({x:Crafty.viewport.width/2-200,y:Crafty.viewport.height/2-20,w:600})
+        .text("Game Over. <br/>Press 'r' to restart")
+        .textColor('red')
+        .textFont({
+          size: '36px',
+          type: 'bold',
+          family: 'Arial'
+        })
+
+
+
+        Crafty.pause();
+
+      }
+    }
+  })
+  })
+  Crafty.load(assetsObj,function(){Crafty.scene('main')});
+})
---
+++ heroes.js
@@ -0,0 +1,460 @@
+Crafty.c("AI", {
+  required: "2D, DOM, Collision, Tween, Color, Motion,Mouse, AngularMotion, Image",
+  fireDelay: 1,
+  los: 600,
+  start: 0,
+  type: 0,
+  diffMod: 1,
+  shielded: false,
+  init: function(team,type){
+    this.init = true;
+    this.w = 30;
+    this.h = 30;
+    this.origin("center")
+    this.healthBar = Crafty.e('Status').attr({x:this.x,y:this.y+30})
+    this.onHit("Arrow",function(hitData){
+      Engine.collide(this,hitData)
+    })
+    this.onHit("Attack",function(hitData){
+      Engine.collide(this,hitData)
+    })
+    this.bind('MouseDown',function(e){
+      if(this.team!=='hero'){
+        if(heroes.length>0){
+          if(abilities[placeIndex].value<gold){
+            var cost =abilities[placeIndex].value;
+            Engine.abilities[abilities[placeIndex].name](this,cost);
+          }
+        }
+      }
+    })
+    this.hitDelay = 10;
+  },
+
+  generic: function(){
+    if(this.init){
+      this.id = 1;
+
+      this.spawn={x:this.x,y:this.y}
+    }
+
+      this.currPos = {
+        x:this.x,
+        y:this.y
+      }
+      var enemies;
+      if(this.team == 'hero'){
+        if(monsters.length)
+          enemies = monsters;
+        else {
+          enemies = [door]
+        }
+      }
+      else{
+          enemies = heroes
+
+
+
+      }
+      this.dist = 0
+      this.targeting = false;
+      for(var i=0;i<enemies.length;i++){
+        var enemy = enemies[i];
+        if(enemy){
+          var dist = Crafty.math.distance(this.currPos.x, this.currPos.y,enemy.x,enemy.y)
+
+          if(dist<this.dist||!this.dist){
+            this.dist = dist
+            this.targetPos = {
+              x:enemy.x,
+              y:enemy.y
+            }
+            if(this.dist<this.los)
+            this.rotation = Engine.getRotation(this.currPos,this.targetPos);
+            this.targeting = true;
+          }
+        }
+
+    }
+  },
+  "ranger": function(){
+    if(this.init){
+      this.range = 400
+      this.maxHealth = Math.round(100*this.diffMod);
+      this.health = this.maxHealth;
+      this.image('ranger.png')
+      this.init = false;
+      this.damage = 20*this.diffMod
+      this.speed =1;
+      this.value=15*this.diffMod
+    }
+    if(this.dist){
+
+      if(this.dist<this.range+1 && this.targeting && this.fireDelay <0){
+        this.cancelTween;
+        var arrowX = this.x+this.w/2;
+        var arrowY = this.y+this.h/2;
+        var arrow = Crafty.e("2D,Color,Collision,Arrow,DOM")
+        .attr({
+            x:arrowX,
+            y:arrowY,
+            w:5,
+            h:30,
+            rotation: this._rotation,
+            xspeed: 7 * Math.sin(this._rotation/57.3),
+            yspeed: 7 * Math.cos(this._rotation/57.3)
+
+          })
+        .color("#533108")
+        arrow.team = this.team;
+        arrow.damage = this.damage
+        var spread= Crafty.math.randomNumber(-0.5,0.5)
+        arrow.bind("EnterFrame",function(){
+          this.x-= this.xspeed+spread
+          this.y+=this.yspeed+spread
+        })
+        Crafty.e("Delay").delay(function() {
+          arrow.destroy();
+
+        }, 2000);
+        this.fireDelay = 35
+
+      }
+      else if(this.dist>this.range&&this.dist<this.los){
+        Engine.moveTo(this,this.speed);
+
+      }
+
+      this.fireDelay--;
+
+    }
+    if(this.targetPos && this.team == 'hero'){
+      this.dist = Crafty.math.distance(this.currPos.x, this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>this.range){
+        Engine.moveTo(this,this.speed)
+      }
+    }
+    if(this.team == 'monster'&&heroes.length==0&&this.currPos!==this.spawn){
+      this.targetPos = this.spawn;
+      this.dist = Crafty.math.distance(this.currPos.x,this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>30)
+        Engine.moveTo(this,this.speed);
+        this.rotation = 0;
+    }
+
+  },
+  "fighter": function(){
+    if(this.init){
+      this.i=2;
+      this.maxHealth = 100*this.diffMod
+      this.health = this.maxHealth;
+      this.image('fighter.png')
+      console.log(this.image)
+      this.range = 50;
+      this.damage = 20*this.diffMod
+      this.init = false
+      this.speed =1.5;
+      this.value=10*this.diffMod;
+      this.onHit("Arrow",function(hitData){
+        Engine.collideShield(hitData,this,1);
+      })
+      this.onHit("Attack",function(hitData){
+        Engine.collideShield(hitData,this,2.5);
+      })
+    }
+    if(this.dist){
+      if(this.dist<=this.range && this.targeting && this.fireDelay < 0){
+        this.fireDelay += 100;
+        var x=this.targetPos.x;
+        var y=this.targetPos.y;
+        var scale = 50;
+        var offset = 20;
+        var tmpDir = Engine.degree({x:x,y:y}, {x:this.x+this.w/2,y:this.y+this.w/2},true);
+        var tmpX = (this.x+this.w/2) + ((this.w/2+scale/2-offset) *  Math.cos(tmpDir-Math.PI/2));
+        var tmpY = this.y+this.w/2 + ((this.w/2+scale/2-offset) * Math.sin(tmpDir+Math.PI/2));
+        var atkHitBox = Crafty.e("2D,Collision,Attack,Delay")
+          .attr({x:tmpX-scale/2,y:tmpY-scale/2,w:scale,h:scale})
+          .origin("center")
+          atkHitBox.team = this.team
+          atkHitBox.damage = this.damage
+          atkHitBox.rotation = this.rotation
+          this.attach(atkHitBox)
+          Crafty.e("Delay").delay(function() {
+            atkHitBox.destroy();
+          }, 200);
+      }
+      else if(this.dist>this.range&&this.dist<this.los){
+        Engine.moveTo(this,this.speed)
+
+      }
+
+        this.fireDelay--;
+
+    }
+    if(this.targetPos&&this.team=='hero'){
+      this.dist = Crafty.math.distance(this.currPos.x, this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>this.range)
+        Engine.moveTo(this,this.speed)
+    }
+    if(this.team == 'monster'&&heroes.length==0&&this.currPos!==this.spawn){
+      this.targetPos = this.spawn;
+      this.dist = Crafty.math.distance(this.currPos.x,this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>this.range)
+        Engine.moveTo(this,this.speed);
+        this.rotation = 0;
+    }
+
+
+
+
+  },
+  "barbarian": function(){
+    if(this.init){
+      this.i=2;
+      this.maxHealth = 250*this.diffMod;
+      this.health = this.maxHealth;
+      this.image('barbarian.png')
+      this.range = 50;
+      this.damage = 40*this.diffMod
+      this.init = false
+      this.speed =2.5;
+      this.value=20*this.diffMod;
+    }
+    if(heroes.length>0){
+      this.health-=0.1
+      this.healthBar.subtract(0.1,this.maxHealth)
+      if(this.health<=0)
+        Engine.death(this)
+    }
+    else{
+      Engine.abilities['heal'](this,0)
+    }
+    if(this.dist){
+      if(this.dist<this.range && this.targeting && this.fireDelay < 0){
+        this.fireDelay += 75;
+        var x=this.targetPos.x;
+        var y=this.targetPos.y;
+        var scale = 50;
+        var offset = 20;
+        var tmpDir = Engine.degree({x:x,y:y}, {x:this.x+this.w/2,y:this.y+this.w/2},true);
+        var tmpX = (this.x+this.w/2) + ((this.w/2+scale/2-offset) *  Math.cos(tmpDir-Math.PI/2));
+        var tmpY = this.y+this.w/2 + ((this.w/2+scale/2-offset) * Math.sin(tmpDir+Math.PI/2));
+        var atkHitBox = Crafty.e("2D,Collision,Attack,Delay")
+          .attr({x:tmpX-scale/2,y:tmpY-scale/2,w:scale,h:scale})
+          .origin("center")
+          atkHitBox.team = this.team
+          atkHitBox.damage = this.damage
+          atkHitBox.rotation = this.rotation
+          this.attach(atkHitBox)
+          Crafty.e("Delay").delay(function() {
+            atkHitBox.destroy();
+          }, 200);
+      }
+      else if(this.dist>this.range&&this.dist<this.los){
+        Engine.moveTo(this,this.speed)
+
+      }
+
+        this.fireDelay--;
+
+    }
+    if(this.targetPos && this.team == 'hero'){
+      this.dist = Crafty.math.distance(this.currPos.x, this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>this.range)
+        Engine.moveTo(this,this.speed)
+    }
+    if(this.team == 'monster'&&heroes.length==0&&this.currPos!==this.spawn){
+      this.targetPos = this.spawn;
+      this.dist = Crafty.math.distance(this.currPos.x,this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>this.range)
+        Engine.moveTo(this,this.speed);
+        this.rotation = 0;
+    }
+  },
+  'cleric': function(){
+    if(this.init){
+      this.range = 400
+      this.maxHealth = parseInt(100*this.diffMod);
+      this.health = this.maxHealth;
+
+      this.init = false;
+      this.damage = 20*this.diffMod
+      this.speed =1;
+      this.value=15*this.diffMod
+      this.healDelay = 50;
+      this.image('cleric.png')
+
+    }
+    if(this.team == 'hero'){
+      this.allies = heroes;
+    }
+    else{
+      this.allies = monsters;
+    }
+    if(this.healDelay<=0&&this.allies.length){
+      this.allies.sort(Engine.sortArray);
+      var lowestAlly = this.allies[0];
+      if(lowestAlly.health/lowestAlly.maxHealth<0.75){
+        Engine.abilities['heal'](lowestAlly,0)
+        this.healDelay = 100;
+      }
+      // else if(this.enemies.length===0){
+      //   Engine.abilities['heal'](lowestAlly,0)
+      //   this.healDelay = 100;
+      // }
+
+    }
+    this.healDelay--;
+
+    if(this.team == 'monster'&&heroes.length==0&&this.currPos!==this.spawn){
+      this.targetPos = this.spawn;
+      this.dist = Crafty.math.distance(this.currPos.x,this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>30)
+        Engine.moveTo(this,this.speed);
+        this.rotation = 0;
+      }
+  },
+  'peasant': function(){
+    if(this.init){
+      this.i=2;
+      this.maxHealth = 21*this.diffMod
+      this.health = this.maxHealth;
+      this.image('peasant.png')
+      this.range = 50;
+      this.damage = 5*this.diffMod
+      this.init = false
+      this.speed =0.7;
+      this.value=10*this.diffMod;
+      this.onHit("Arrow",function(hitData){
+        Engine.collideShield(hitData,this,1);
+      })
+      this.onHit("Attack",function(hitData){
+        Engine.collideShield(hitData,this,2.5);
+      })
+    }
+    if(this.dist){
+      if(this.dist<this.range+10 && this.targeting && this.fireDelay < 0){
+        this.fireDelay += 100;
+        var x=this.targetPos.x;
+        var y=this.targetPos.y;
+        var scale = 50;
+        var offset = 20;
+        var tmpDir = Engine.degree({x:x,y:y}, {x:this.x+this.w/2,y:this.y+this.w/2},true);
+        var tmpX = (this.x+this.w/2) + ((this.w/2+scale/2-offset) *  Math.cos(tmpDir-Math.PI/2));
+        var tmpY = this.y+this.w/2 + ((this.w/2+scale/2-offset) * Math.sin(tmpDir+Math.PI/2));
+        var atkHitBox = Crafty.e("2D,Collision,Attack,Delay")
+          .attr({x:tmpX-scale/2,y:tmpY-scale/2,w:scale,h:scale})
+          .origin("center")
+          atkHitBox.team = this.team
+          atkHitBox.damage = this.damage
+          atkHitBox.rotation = this.rotation
+          this.attach(atkHitBox)
+          Crafty.e("Delay").delay(function() {
+            atkHitBox.destroy();
+          }, 200);
+      }
+      else if(this.dist>this.range&&this.dist<this.los){
+        Engine.moveTo(this,this.speed)
+
+      }
+
+        this.fireDelay--;
+
+    }
+    if(this.targetPos&&this.team=='hero'){
+      this.dist = Crafty.math.distance(this.currPos.x, this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>this.range)
+        Engine.moveTo(this,this.speed)
+    }
+    if(this.team == 'monster'&&heroes.length==0&&this.currPos!==this.spawn){
+      this.targetPos = this.spawn;
+      this.dist = Crafty.math.distance(this.currPos.x,this.currPos.y,this.targetPos.x,this.targetPos.y)
+      if(this.dist>this.range)
+        Engine.moveTo(this,this.speed);
+        this.rotation = 0;
+    }
+  },
+  events: {
+    "EnterFrame": function(){
+      this.generic();
+      this[this.type]();
+      this.healthBar.x = this.x;
+      this.healthBar.y = this.y+40
+    }
+  }
+})
+
+Crafty.c('Status',{
+  init: function(){
+    this.requires('2D, DOM, Color, Text, Tween');
+    this.w = 30;
+    this.h = 3;
+    this.maxWidth = 30;
+    this.status=100;
+    this.color("#ff0000");
+
+  },
+  subtract: function(num,obj){
+    var fraction = (num/obj)*this.maxWidth;
+    this.w -=fraction;
+    if(this.w<0){
+      this.w=0
+    }
+    if(this.w>this.maxWidth){
+      this.w = this.maxWidth
+    }
+
+  },
+  events: {
+    "EnterFrame": function(){
+    }
+  }
+
+})
+Crafty.c('Gold',{
+
+  init: function(){
+    this.requires("2D, DOM, Text, Color")
+    this.x = Crafty.viewport.width-75
+    this.y = 30
+    this.w = 200
+    this.gold = 0
+    this.goldText = "Gold - "+this.gold
+    this.text(this.goldText)
+    this.textColor('#bea107')
+    this.textFont({
+      size: '20px',
+      type: 'bold',
+      family: 'Arial'
+    })
+  },
+  events: {
+    'EnterFrame':function(){
+      this.goldText = this.gold+'GP';
+      this.text(this.goldText);
+    }
+  }
+})
+Crafty.c('Selected',{
+  required: '2D, DOM, Text, Color',
+  init:function(){
+    this.x = Crafty.viewport.width-200
+    this.y = Crafty.viewport.height-35
+    this.w = 200;
+    this.text('fighter')
+    this.textColor('#fffc00');
+    this.textFont({
+      size: '20px',
+      type: 'bold',
+      family: 'Arial'
+    })
+  },
+  events:{
+    "EnterFrame":function(){
+      if(heroes.length>0)
+        this.text('['+abilities[placeIndex].name.toUpperCase()+"- "+abilities[placeIndex].value+']')
+      if(heroes.length===0)
+        this.text('['+store[placeIndex].name.toUpperCase()+"- "+store[placeIndex].value+']')
+    }
+  }
+})
